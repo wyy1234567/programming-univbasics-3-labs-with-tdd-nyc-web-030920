1ba3198571a3c5349d688d174536d274151d1692
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-16 19:20:51 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
 # Add your variables here
+first_number = 10
+second_number = 20
+quotient = first_number / second_number
+product = first_number * second_number
+sum = first_number + second_number
+difference = first_number - second_number
